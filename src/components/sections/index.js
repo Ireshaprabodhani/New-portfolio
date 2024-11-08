7fe8ae051e6101024a27909c15d8b012bc375240
Ireshaprabodhani <84035631+Ireshaprabodhani@users.noreply.github.com>
2024-11-08 14:27:10 +0530

    remove all files

--- src/components/sections/index.js
+++
@@ -1,6 +0,0 @@
-// Make sure you have an index.js file in your components/sections folder:
-// src/components/sections/index.js
-export { default as Hero } from './Hero';
-export { default as About } from './About';
-export { default as Projects } from './Projects';
-export { default as Contact } from './Contact';
